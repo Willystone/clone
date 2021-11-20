530634db8cbbd6e93e82a50833bbab1dd7f9422c
Willystone <willstown@icloud.com>
2021-11-20 22:30:08 +0300

    prototype file added

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -1,69 +1,30 @@
 #include "main.h"
 
 /**
- * str_len - Finds length of string.
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
  *
- * @s: String of which length is to be found.
- *
- * Return: The length of the string;
- */
-int str_len(const char *s)
-{
-	int len = 0, idx;
-
-	for (idx = 0; *(s + idx); idx++)
-	{
-		if (*(s + idx) == '\0')
-			break;
-		len++;
-	}
-	return (len);
-}
-
-/**
- * power - Finds Power @y of @x.
- *
- * @x: Base number.
- * @y: Exponent.
- *
- * Return: x raised to power y.
- */
-int power(int x, int y)
-{
-	int pow = 0;
-
-	if (y == 0)
-		return (1);
-
-	pow = x * power(x, y - 1);
-
-	return (pow);
-}
-
-/**
- * binary_to_uint - Converts binary number to unsigned int.
- *
- * @b: Pointer to string of 0 and 1 chars.
- *
- * Return: The converted number, 0 if b is NULL.
- *	   0 if there's one or more chars in b != 0 or 1.
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = str_len(b);
-	unsigned int dec = 0, idx = 0;
+int i;
+	unsigned int num;
 
-	if (b == NULL)
+	num = 0;
+	if (!b)
 		return (0);
-
-	while (len > 0)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		--len;
-		if (*(b + len) != '0' && *(b + len) != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		dec += (*(b + len) - '0') * power(2, idx);
-		idx++;
 	}
-	return (dec);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
